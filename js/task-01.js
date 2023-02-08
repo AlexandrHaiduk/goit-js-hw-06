ec24faefc2e57dfbc397b818374dd7e24cb0bfff
AlexandrHaiduk <alexanderhaiduk@gmail.com>
2023-02-08 23:28:55 +0200

    tast 1 done

---
+++ js/task-01.js
@@ -0,0 +1,18 @@
+const categories = document.querySelectorAll('.item');
+logInformation(categories)
+
+
+
+
+function logInformation(categoryList) {
+    console.log(`Number of categories: ${categories.length}`)
+    console.log("")
+    categoryList.forEach(category => {
+        const categoryTitle = category.querySelector('h2')
+        const categoryElements = category.querySelectorAll('li')
+        console.log(`Category: ${categoryTitle.textContent}`)
+        console.log(`Elements: ${categoryElements.length}`)
+        console.log("")
+})
+}
+

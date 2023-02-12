ccb74c5b1d947b421f8c00c03ff67304bb594df8
AlexandrHaiduk <alexanderhaiduk@gmail.com>
2023-02-12 20:47:52 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,36 @@
+const boxesQuantityEl = document.querySelector('input');
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const boxesEl = document.querySelector('#boxes')
+
+
+createBtn.addEventListener("click", () => {
+
+  let width = 30;
+  let height = 30;
+
+  for (let i = 0; i < boxesQuantityEl.value; i += 1) {
+    let newBox = document.createElement('div');
+
+    newBox.style.width = width + "px";
+    newBox.style.height = height + "px";
+    newBox.style.backgroundColor = getRandomHexColor()
+    width += 10;
+    height += 10;
+    boxesEl.append(newBox)
+  }
+})
+
+destroyBtn.addEventListener('click', () => {
+  boxesEl.innerHTML = ""
+})
+
+
+
+
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }

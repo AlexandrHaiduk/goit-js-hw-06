a7b3164e2430e54f33f0b508008a1121e5c24c4b
AlexandrHaiduk <alexanderhaiduk@gmail.com>
2023-02-12 20:07:44 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const changeColorBtn = document.querySelector('.change-color')
+const colorInfoEl = document.querySelector('.color')
+
+changeColorBtn.addEventListener('click', event => {
+  document.body.style.backgroundColor = getRandomHexColor()
+  colorInfoEl.textContent = document.body.style.backgroundColor
+})
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }

852b62d1dcb90315770f2201eddf71e05fb263c7
AlexanderHaiduk <alexanderhaiduk@gmail.com>
2023-02-14 18:49:21 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,13 @@
 const inputEl = document.querySelector("#validation-input");
 
-const lengthValue = inputEl.getAttribute("data-length")
+const lengthValue = +inputEl.getAttribute("data-length")
 
 inputEl.addEventListener('blur', event => {
     event.currentTarget.classList.remove('invalid');
     event.currentTarget.classList.remove('valid');
 
 
-    if (event.currentTarget.value.length == lengthValue) { // не розумію чого не працює строге порівнняння "==="?
+    if (event.currentTarget.value.length === lengthValue) { // не розумію чого не працює строге порівнняння "==="?
         event.currentTarget.classList.add("valid")
     } else {
         event.currentTarget.classList.add('invalid')

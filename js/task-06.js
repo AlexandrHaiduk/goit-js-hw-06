d50ed70c76d5f7c80e2f708af5a85b58bdec60dc
AlexanderHaiduk <alexanderhaiduk@gmail.com>
2023-02-14 14:38:31 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -5,8 +5,7 @@ const lengthValue = inputEl.getAttribute("data-length")
 inputEl.addEventListener('blur', event => {
     event.currentTarget.classList.remove('invalid');
     event.currentTarget.classList.remove('valid');
-    console.log("lengthValue ",lengthValue)
-    console.log("event.currentTarget.value.length ",event.currentTarget.value.length)
+
 
     if (event.currentTarget.value.length == lengthValue) {
         event.currentTarget.classList.add("valid")
